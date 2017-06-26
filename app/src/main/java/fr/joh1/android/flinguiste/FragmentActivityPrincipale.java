187a4d4a426841c108bd3b2e28eb07b3523450f0
joH1 <jiema@live.fr>
2017-06-26 23:42:36 +0200

    Petit changement de package où aller récupérer la classe `SimpleCursorAdapter`

--- app/src/main/java/fr/joh1/android/flinguiste/FragmentActivityPrincipale.java
+++ app/src/main/java/fr/joh1/android/flinguiste/FragmentActivityPrincipale.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.widget.SimpleCursorAdapter;
+import android.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
