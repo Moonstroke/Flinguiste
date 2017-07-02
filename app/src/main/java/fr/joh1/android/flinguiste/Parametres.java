226312975dc2bc07cce7038726753d218120871b
joH1 <jiema@live.fr>
2017-07-02 22:52:26 +0200

    Ajout de valeurs par défaut pour les paramètres

--- app/src/main/java/fr/joh1/android/flinguiste/Parametres.java
+++ app/src/main/java/fr/joh1/android/flinguiste/Parametres.java
@@ -8,12 +8,14 @@ class Parametres {
 
 	/**
 	 * Le nombre de propositions par question
+	 * Par défaut 4, c'est un choix légitime
 	 */
-	public static int choix;
+	public static int choix = 4;
 
 	/**
 	 * Le nombre de questions par partie
+	 * Par défaut 10 c'est de même un choix tout à fait raisonnable
 	 */
-	public static int total;
+	public static int total = 10;
 
 }
