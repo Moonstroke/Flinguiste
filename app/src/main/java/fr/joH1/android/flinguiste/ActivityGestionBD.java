ecec43b4c9b4a88137a636db64c12333e012524b
joH1 <jiema@live.fr>
2017-07-08 01:16:30 +0200

    Modifié légèrement les `Spinners`

--- app/src/main/java/fr/joH1/android/flinguiste/ActivityGestionBD.java
+++ app/src/main/java/fr/joH1/android/flinguiste/ActivityGestionBD.java
@@ -1,6 +1,7 @@
 package fr.joH1.android.flinguiste;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
@@ -28,13 +29,10 @@ public class ActivityGestionBD extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-
-		Context ctx = getApplicationContext();
-
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_gestion_bd);
 
-		assistantSQLite = new AssistantSQLite(ctx, false);
+		assistantSQLite = new AssistantSQLite(this, false);
 
 
 		sAjMotNiv = (Spinner)findViewById(R.id.s_aj_mot_niv);
@@ -46,24 +44,27 @@ public class ActivityGestionBD extends AppCompatActivity {
 		etAjDefDef = (AppCompatEditText)findViewById(R.id.et_aj_def_def);
 
 
-		String colType = "type";
-		SimpleCursorAdapter adaptateur = new SimpleCursorAdapter(ctx, android.R.layout.simple_spinner_dropdown_item, assistantSQLite.types(colType, false), new String[] {colType}, new int[] {android.R.id.text1}, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
-		adaptateur.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		Resources ressources = getResources();
 
-		sAjMotType.setAdapter(adaptateur);
-		sAjMotType.setPrompt(ctx.getResources().getString(R.string.texte_aj_mot_type));
-		adaptateur.changeCursor(assistantSQLite.niveaux(colType, true));
-		sAjDefType.setAdapter(adaptateur);
-		sAjDefType.setPrompt(ctx.getResources().getString(R.string.texte_aj_def_type));
 		String colNiv = "niveau";
-		adaptateur.changeCursorAndColumns(assistantSQLite.niveaux(colNiv, false), new String[] {colNiv}, new int[] {1});
-		sAjMotNiv.setAdapter(adaptateur);
-		sAjMotNiv.setPrompt(ctx.getResources().getString(R.string.texte_aj_mot_niv));
+		SimpleCursorAdapter adaptateurNiveaux = new SimpleCursorAdapter(this, android.R.layout.simple_spinner_dropdown_item, assistantSQLite.niveaux(colNiv, false), new String[] {colNiv}, new int[] {android.R.id.text1}, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+		adaptateurNiveaux.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		sAjMotNiv.setAdapter(adaptateurNiveaux);
+		sAjMotNiv.setPrompt(ressources.getString(R.string.texte_aj_mot_niv));
+
+		String colType = "type";
+		SimpleCursorAdapter adaptateurTypes = new SimpleCursorAdapter(this, android.R.layout.simple_spinner_dropdown_item, assistantSQLite.types(colType, false), new String[] {colType}, new int[] {android.R.id.text1}, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+		adaptateurTypes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		sAjMotType.setAdapter(adaptateurTypes);
+		sAjMotType.setPrompt(ressources.getString(R.string.texte_aj_mot_type));
+
+		sAjDefType.setAdapter(adaptateurTypes);
+		sAjDefType.setPrompt(ressources.getString(R.string.texte_aj_def_type));
 
 	}
 
 	public void ajMot(View v) {
-
 		String mot = String.valueOf(etAjMotMot.getText());
 		int niveau = sAjMotNiv.getSelectedItemPosition() + 1;
 		int type = sAjMotType.getSelectedItemPosition() + 1;
@@ -73,7 +74,6 @@ public class ActivityGestionBD extends AppCompatActivity {
 	}
 
 	public void ajDef(View v) {
-
 		int type = sAjDefType.getSelectedItemPosition() + 1;
 		String def = String.valueOf(etAjDefDef.getText());
 
